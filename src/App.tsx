2dd2f8e551ea807f6371b5c5cb2f6be771367821
duhyo08 <duhyo08@dsg.mz.co.kr>
2023-12-25 22:52:38 +0900

    fix conflict

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import SideNavBar from "./components/SideNavBar";
-<<<<<<< HEAD
-import styled from "styled-components";
-import Toppings from "./components/menu/Toppings";
-import Order from "./components/menu/Order";
-=======
->>>>>>> 02bf61f2c9eee019651573fcca15ae90ee5557b7
 
 function App() {
   return (
